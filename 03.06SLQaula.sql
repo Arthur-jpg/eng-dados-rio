d310a4cb566c970725530c7ea1eec0435dd81aad
ARTHUR PEIXOTO SCHILLER <202402851756@alunos.ibmec.edu.br>
2024-06-03 17:13:28 -0300

    view continuacao aula 03.06

--- 03.06SLQaula.sql
+++ 03.06SLQaula.sql
@@ -43,8 +43,56 @@ insert into produto (idproduto, nomeproduto,idmarca,preco)
 values
 (11, 'Milho em Lata', 3, 3.60),
 (12, 'ervilha em Lata',2, 3.90),
-(13, 'Sardinha em Lata',1,4.90)
+(13, 'Sardinha em Lata',1,4.90);
 
 -- ex 25
+select cliente.nomecliente
+from cliente
+where cliente.idcliente not in (select count(compras.idcompra) as total
+from compras
+having total > 0
+order by cliente.idcliente);
+
+-- correção 
+select idcliente, nomecliente
+from cliente
+where idcliente not in (
+	select distinct cliente.idcliente
+	from cliente,compras
+	where cliente.idcliente = compras.idcliente
+);
+
+-- ex 26
+select nomeproduto
+from produto
+where nomeproduto not in(
+	select distinct pr.nomeproduto
+	from produto as pr, compras as co
+	where pr.idproduto = co.idproduto
+	and year(dtcompra) = '2021'
+);
 
 
+-- visão
+-- ex 27
+create or replace view vw_produtolista as
+(
+select produto.idproduto, produto.nomeproduto, produto.preco, marca.nomemarca
+from produto, marca
+where produto.idmarca = marca.idmarca
+order by produto.idproduto asc
+);
+
+select * from vw_produtolista;
+
+
+-- ex 28
+create or replace view vw_compraslista as 
+(
+select compras.idcompra, cliente.nomecliente, produto.nomeproduto, marca.nomemarca, loja.nomeloja, compras.dtcompra, compras.qtd, produto.preco, compras.qtd*produto.preco as valor
+from compras, cliente, produto, marca, loja
+where compras.idproduto = produto.idproduto and compras.idcliente = cliente.idcliente and compras.idloja = loja.idloja and produto.idmarca = marca.idmarca
+order by compras.idcompra
+);
+
+select sum(valor) from vw_compraslista;
